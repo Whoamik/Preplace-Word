2c076b1d9f07467c79f43f0447c02954df3d060e
whoami <vuong.tan.nhac@gmail.com>
2018-03-21 14:37:25 +0700

    word

---
+++ Word/frmReplaceWord.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Word
+{
+    partial class frmReplaceWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(477, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnIn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 14);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(459, 347);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "GMC";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 373);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "In Word";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Word/frmReplaceWord.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+using word = Microsoft.Office.Interop.Word;
+using System.Collections.Generic;
+using Novacode;
+using System.IO;
+
+namespace Word
+{
+    public partial class frmReplaceWord : Form
+    {
+        public frmReplaceWord()
+        {
+            InitializeComponent();
+        }
+        private void btnIn_Click(object sender, EventArgs e)
+        {
+            List<DataWord> sData = new List<DataWord>();
+            sData.Add(new DataWord("[STT]", "NHAC_"));
+            sData.Add(new DataWord("[ChiNhanh]", "NHAC_"));
+            sData.Add(new DataWord("[BankName]", "NHAC_"));
+            sData.Add(new DataWord("[CustomerName]", "NHAC_"));
+            sData.Add(new DataWord("[CustomerAddress]", "NHAC_"));
+            sData.Add(new DataWord("[BankNo]", "NHAC_"));
+            sData.Add(new DataWord("[Date]", "NHAC_"));
+            sData.Add(new DataWord("[TotalAmt]", "NHAC_"));
+            sData.Add(new DataWord("[PayReqDate]", "NHAC_"));
+            sData.Add(new DataWord("[LoanNo_OverDueAmt_OverDueDate]", "NHAC_"));
+            sData.Add(new DataWord("[EmployessName]", "NHAC_"));
+            sData.Add(new DataWord("[Position]", "NHAC_"));
+            sData.Add(new DataWord("[Phone]", "NHAC_"));
+            word.Application wordApp = new word.Application();
+            String StartupPath = System.Windows.Forms.Application.StartupPath + "\\VPB-TBLAN1.docx";
+            if (!File.Exists(StartupPath))
+            {
+                MessageBox.Show("Không tìm thấy file Template", "Thông Báo");
+                return;
+            }
+            else
+            {
+                Document newDocument = wordApp.Documents.Open(StartupPath);
+                try
+                {
+                    foreach (DataWord dt in sData)
+                    {
+                        while (newDocument.Content.Find.Execute(FindText: dt.K_Name))
+                        {
+                            if (newDocument.Content.Find.Execute(FindText: dt.K_Name))
+                            {
+                                newDocument.Content.Find.Execute(FindText: dt.K_Name, ReplaceWith: dt.K_Replace, Replace: WdReplace.wdReplaceOne);
+                            }
+                        }
+                        richTextBox1.Text = newDocument.Content.Text.ToString();
+                    }
+
+                    newDocument.SaveAs(System.Windows.Forms.Application.StartupPath + "\\Temp.docx");
+                    newDocument.Close();
+                }
+                catch (Exception)
+                {
+                    newDocument.SaveAs(System.Windows.Forms.Application.StartupPath + "\\Temp (1).docx");
+                    newDocument.Close();
+                }
+            }
+        }
+    }
+    public class DataWord
+    {
+        private String Name;
+        private String Replace;
+
+        public DataWord(String K_Name, String K_Replace)
+        {
+            Name = K_Name;
+            Replace = K_Replace;
+        }
+
+        public String K_Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public String K_Replace
+        {
+            get { return Replace; }
+            set { Replace = value; }
+        }
+    }
+}
