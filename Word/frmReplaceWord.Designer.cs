9cef005478463388cbd3971637148ded750f272b
whoami <vuong.tan.nhac@gmail.com>
2018-03-22 11:53:37 +0700

    killProcesses
    #killProcesses

--- Word/frmReplaceWord.Designer.cs
+++ Word/frmReplaceWord.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -52,14 +53,25 @@
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "GMC";
             // 
-            // Form1
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(477, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 60);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kill Processes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmReplaceWord
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(676, 373);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.Name = "frmReplaceWord";
             this.Text = "In Word";
             this.ResumeLayout(false);
 
@@ -70,6 +82,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Word/frmReplaceWord.cs
+++ Word/frmReplaceWord.cs
@@ -5,6 +5,7 @@ using word = Microsoft.Office.Interop.Word;
 using System.Collections.Generic;
 using Novacode;
 using System.IO;
+using System.Diagnostics;
 
 namespace Word
 {
@@ -14,24 +15,16 @@ namespace Word
         {
             InitializeComponent();
         }
+        private List<DataWord> sData = new List<DataWord>();
+        String name_file = "VPB-TBLAN1.docx";
         private void btnIn_Click(object sender, EventArgs e)
         {
-            List<DataWord> sData = new List<DataWord>();
-            sData.Add(new DataWord("[STT]", "NHAC_"));
-            sData.Add(new DataWord("[ChiNhanh]", "NHAC_"));
-            sData.Add(new DataWord("[BankName]", "NHAC_"));
-            sData.Add(new DataWord("[CustomerName]", "NHAC_"));
-            sData.Add(new DataWord("[CustomerAddress]", "NHAC_"));
-            sData.Add(new DataWord("[BankNo]", "NHAC_"));
-            sData.Add(new DataWord("[Date]", "NHAC_"));
-            sData.Add(new DataWord("[TotalAmt]", "NHAC_"));
-            sData.Add(new DataWord("[PayReqDate]", "NHAC_"));
-            sData.Add(new DataWord("[LoanNo_OverDueAmt_OverDueDate]", "NHAC_"));
-            sData.Add(new DataWord("[EmployessName]", "NHAC_"));
-            sData.Add(new DataWord("[Position]", "NHAC_"));
-            sData.Add(new DataWord("[Phone]", "NHAC_"));
+            CreateData();
             word.Application wordApp = new word.Application();
-            String StartupPath = System.Windows.Forms.Application.StartupPath + "\\VPB-TBLAN1.docx";
+            //String name_file = "VPB-TBLAN1.docx";
+            String StartupPath = System.Windows.Forms.Application.StartupPath + "\\" + name_file;
+            Document newDocument = wordApp.Documents.Open(StartupPath, ReadOnly: true);
+            wordApp.Visible = false;
             if (!File.Exists(StartupPath))
             {
                 MessageBox.Show("Không tìm thấy file Template", "Thông Báo");
@@ -39,7 +32,6 @@ namespace Word
             }
             else
             {
-                Document newDocument = wordApp.Documents.Open(StartupPath);
                 try
                 {
                     foreach (DataWord dt in sData)
@@ -59,10 +51,107 @@ namespace Word
                 }
                 catch (Exception)
                 {
-                    newDocument.SaveAs(System.Windows.Forms.Application.StartupPath + "\\Temp (1).docx");
+                    newDocument.SaveAs(System.Windows.Forms.Application.StartupPath + "\\Temp " + DateTime.Now.ToString("HHmmss") + ".docx");
                     newDocument.Close();
                 }
             }
+        }
+        private void CreateData()
+        {
+            sData.Add(new DataWord("[STT]", "NHAC_"));
+            sData.Add(new DataWord("[ChiNhanh]", "NHAC_"));
+            sData.Add(new DataWord("[BankName]", "NHAC_"));
+            sData.Add(new DataWord("[CustomerName]", "NHAC_"));
+            sData.Add(new DataWord("[CustomerAddress]", "NHAC_"));
+            sData.Add(new DataWord("[BankNo]", "NHAC_"));
+            sData.Add(new DataWord("[Date]", "NHAC_"));
+            sData.Add(new DataWord("[TotalAmt]", "NHAC_"));
+            sData.Add(new DataWord("[PayReqDate]", "NHAC_"));
+            sData.Add(new DataWord("[LoanNo_OverDueAmt_OverDueDate]", "NHAC_"));
+            sData.Add(new DataWord("[EmployessName]", "NHAC_"));
+            sData.Add(new DataWord("[Position]", "NHAC_"));
+            sData.Add(new DataWord("[Phone]", "NHAC_"));
+        }
+        public List<Processes> GetRunningProcesses()
+        {
+            List<Processes> ProcessList = new List<Processes>();
+            //here we're going to get a list of all running processes on
+            //the computer
+            foreach (Process clsProcess in Process.GetProcesses())
+            {
+                if (Process.GetCurrentProcess().Id == clsProcess.Id)
+                    continue;
+                if (clsProcess.ProcessName.Contains("WINWORD"))
+                {
+                    ProcessList.Add(new Processes(clsProcess.Id, clsProcess.MainWindowTitle.ToString()));
+                }
+            }
+            return ProcessList;
+        }
+        private void KillProcesses(List<Processes> processesbeforegen, List<Processes> processesaftergen)
+        {
+            foreach (Processes pidafter in processesaftergen)
+            {
+                bool processfound = false;
+                foreach (Processes pidbefore in processesbeforegen)
+                {
+                    if (pidafter.K_ID == pidbefore.K_ID)
+                    {
+                        processfound = true;
+                    }
+                }
+
+                if (processfound == false)
+                {
+                    Process clsProcess = Process.GetProcessById(pidafter.K_ID);
+                    clsProcess.Kill();
+                }
+            }
+        }
+        private void KillProcesses(List<Processes> LstProcess, String name_file)
+        {
+
+            foreach (Processes proc in LstProcess)
+            {
+                if (proc.K_NAME.Contains(name_file))
+                {
+                    Process clsProcess = Process.GetProcessById(proc.K_ID);
+                    clsProcess.Kill();
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //List<Processes> processesbeforegen = GetRunningProcesses(); // lấy danh sách trước khi code 
+            //// APP CREATION/ DOCUMENT CREATION HERE...
+            //List<Processes> processesaftergen = GetRunningProcesses(); // lấy danh sách sau khi code
+            // KillProcesses(processesbeforegen, processesaftergen); // kill process phát sinh
+            List<Processes> LstProcess = GetRunningProcesses();
+            KillProcesses(LstProcess, name_file);
+        }
+    }
+    public class Processes
+    {
+        private int ID;
+        private String NAME;
+
+        public Processes(int K_ID, String K_NAME)
+        {
+            ID = K_ID;
+            NAME = K_NAME;
+        }
+
+        public int K_ID
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+        public String K_NAME
+        {
+            get { return NAME; }
+            set { NAME = value; }
         }
     }
     public class DataWord
